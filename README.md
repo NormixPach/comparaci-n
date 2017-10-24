53c8dc6ce592409c15fa1b1394162a2b90c920d5
NormixPach <normpach30@gmail.com>
2017-10-24 09:42:40 -0500

    Subiendo mi primer READMED.md

---
+++ README.md
@@ -0,0 +1,9 @@
+1 Crear una función que reciba 2 arreglos
+2 Comprobar si los 2 arreglos tienen la misma longitud
+  2.1 si no son iguales, el programa debe regresar false
+  2.2 si son iguales va a continuar con el proceso
+3 comprobar uno a uno, los valores
+  3.1 si no son iguales todos regresan false
+  3.2 si son exactamente iguales, continuar con el proceso
+4 si hemos llegado a este punto,regresa verdadedo
+5 fin
---
+++ app.js
@@ -0,0 +1,16 @@
+var comparar = function (arreglo1,arreglo2){
+  if(arreglo1.length !== arreglo2.length){
+    return false;
+  }else{
+    for(var i = 0; i < arreglo1.length; i +=1){
+      if(arreglo1[i] !== arreglo2[i]){
+        return false;
+      }
+    }
+    document.write("Todas las condiciones se cumplen");
+    return true;
+  }
+}
+var array1 = [1,2,3,4,5,6,7,8,9,10];
+var array2 = [1,2,3,4,5,6,7,8,9,10];
+comparar(array1,array2);
